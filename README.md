170555b47d92d01301e496a0056d05a3f93bc4b6
Oto Brglez <otobrglez@gmail.com>
2015-12-16 22:57:48 +0100

    Some instructions.

--- README.md
+++ README.md
@@ -4,4 +4,25 @@ Linear and other regressions implemented in Scala.
 
 [![Build Status](https://travis-ci.org/otobrglez/regressor.svg?branch=master)](https://travis-ci.org/otobrglez/regressor)
 
+## Usage
+
+```scala
+val results = Regression.linear(
+    ArrayBuffer(1, 2, 4, 5, 10, 20),
+    ArrayBuffer(4, 6, 12, 15, 34, 68)
+)  
+  
+println(results)
+
+//_1 => slope
+//_2 => intercept
+//_2 => r^2
+
+// Linear function:
+// y = slope*x - intercept
+
+```
+
+## Author
+
 - [Oto Brglez](https://github.com/otobrglez)
--- src/main/scala/Regression.scala
+++ src/main/scala/Regression.scala
@@ -1,4 +1,8 @@
+import scala.concurrent.duration.Duration
 import scala.math.{pow}
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object Regression {
   def linear(xArray: Iterable[Double], yArray: Iterable[Double]) = {
@@ -8,26 +12,55 @@ object Regression {
     require(n == nY, "Collections must have the same length!")
 
     val pairs = xArray zip yArray
-    val sumX = pairs.par.foldLeft(0.0)((s, p) => s + p._1)
-    val sumY = pairs.par.foldLeft(0.0)((s, p) => s + p._2)
-    val sumX2 = pairs.par.foldLeft(0.0)((s, p) => s + pow(p._1, 2))
-    val sumY2 = pairs.par.foldLeft(0.0)((s, p) => s + pow(p._2, 2))
-    val sumXY = pairs.par.foldLeft(0.0)((s, p) => s + p._1 * p._2)
+
+    val sums = for {
+      sumXi <- Future {
+        pairs.foldLeft(0.0)((s, p) => s + p._1)
+      }
+      sumYi <- Future {
+        pairs.foldLeft(0.0)((s, p) => s + p._2)
+      }
+      sumX2i <- Future {
+        pairs.foldLeft(0.0)((s, p) => s + pow(p._1, 2))
+      }
+      sumY2i <- Future {
+        pairs.foldLeft(0.0)((s, p) => s + pow(p._2, 2))
+      }
+      sumXYi <- Future {
+        pairs.foldLeft(0.0)((s, p) => s + p._1 * p._2)
+      }
+
+    } yield (sumXi, sumYi, sumX2i, sumY2i, sumXYi)
+
+    val (sumX, sumY, sumX2, sumY2, sumXY) = Await.result(sums, Duration.Inf)
 
     val dn = n * sumX2 - pow(sumX, 2)
     assert(dn != 0.0, "Can't solve the system!")
 
+    val poms = for {
+      slopei <- Future {
+        ((n * sumXY) - (sumX * sumY)) / dn
+      }
+      intercepti <- Future {
+        ((sumY * sumX2) - (sumX * sumXY)) / dn
+      }
+      t1i <- Future {
+        ((n * sumXY) - (sumX * sumY)) * ((n * sumXY) - (sumX * sumY))
+      }
+      t2i <- Future {
+        (n * sumX2) - pow(sumX, 2)
+      }
+      t31 <- Future {
+        (n * sumY2) - pow(sumY, 2)
+      }
 
-    val slope = ((n * sumXY) - (sumX * sumY)) / dn
-    val intercept = ((sumY * sumX2) - (sumX * sumXY)) / dn
+    } yield (slopei, intercepti, t1i, t2i, t31)
 
-    val t1 = ((n * sumXY) - (sumX * sumY)) * ((n * sumXY) - (sumX * sumY))
-    val t2 = (n * sumX2) - pow(sumX, 2)
-    val t3 = (n * sumY2) - pow(sumY, 2)
+    val (slope, intercept, t1, t2, t3) = Await.result(poms, Duration.Inf)
 
-    var r2 = 0.0
-    if ((t2 * t3) != 0.0) r2 = t1 / (t2 * t3)
-
-    (slope, intercept, r2)
+    if (t2 * t3 != 0.0)
+      (slope, intercept, t1 / (t2 * t3))
+    else
+      (slope, intercept, 0.0)
   }
 }
